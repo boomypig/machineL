cd4051e91d47ce67725cf56b964c3719940a2ac1
Bryan Lopez <brainalexis119@gmail.com>
2026-03-01 17:35:41 -0700

    tune param now

--- pract6/hyperparam.py
+++ pract6/hyperparam.py
@@ -8,17 +8,18 @@ from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.impute import SimpleImputer
+from sklearn.metrics import confusion_matrix, classification_report
 
 def main():
     df = pd.read_csv("./telco_churn.csv")
-    print(df.dtypes)
+    # print(f"{df.dtypes}\n")
     TARGET_COLUMN = "Churn"
     y = df[TARGET_COLUMN]
     x_df = df.drop(columns = [TARGET_COLUMN])
     test_split = 0.15
     val_split = 0.15
     random_state = 119
-    print(x_df["TotalCharges"].head())
+    # print(x_df["TotalCharges"].head(),"\n")
     # changes is a str when it's supposed to be int
     x_df["TotalCharges"] = pd.to_numeric(x_df["TotalCharges"],errors="coerce")
 
@@ -33,26 +34,25 @@ def main():
             changed_to_binary.append(col)
 
             # print(x_df[col].value_counts())
-    print(x_df["TotalCharges"].dtype)
-    print(x_df["TotalCharges"].isna().sum())
+    # print(x_df["TotalCharges"].dtype, "\n")
+    # print(x_df["TotalCharges"].isna().sum(), "\n")
 
-    print(x_df.dtypes.value_counts())
 
-    print(changed_to_binary)
-    
+    # print(f"these columns have binary values: \n {changed_to_binary} \n")
 
     x_trainval, x_test, y_trainval, y_test = train_test_split(x_df,y,test_size=test_split, random_state=random_state)
 
     val_fraction = val_split/(1-test_split)
+
     x_train,x_val,y_train,y_val = train_test_split(x_trainval,y_trainval,test_size=val_fraction,random_state=random_state)            
-    print(f"these columns have binary values: \n {changed_to_binary}")
+
     numeric_col = [c for c in x_train.columns if is_numeric_dtype(x_train[c])]
     cat_col = [c for c in x_train.columns if c not in numeric_col]
 
-    numeric_pipeline = Pipeline(steps=[(
-        ("imputer",SimpleImputer(strategy="median"))
+    numeric_pipeline = Pipeline(steps=[
+        ("imputer",SimpleImputer(strategy="median")),
         ("scaler",StandardScaler())
-    )])
+    ])
 
     cat_pipeline = Pipeline(steps=[
         ("imputer",SimpleImputer(strategy="most_frequent")),
@@ -60,10 +60,9 @@ def main():
     ])
 
     pre = ColumnTransformer(transformers=[
-        ("num_pipe", numeric_pipeline()),
-        ("col_pipe", cat_pipeline())
-    ]remainder="drop")
-
+        ("num_pipe", numeric_pipeline, numeric_col),
+        ("cat_pipe", cat_pipeline, cat_col)
+    ])
 
     clf = Pipeline([('pre', pre), ("model", LogisticRegression())])
     clf.fit(x_train,y_train)
@@ -71,9 +70,7 @@ def main():
     t= .455
     y_hat = (probs_val >= t).astype(int)
     print(clf.classes_)
-    # print(prob[:5])
-    # print("predict:" , y_hat[:5])
-    # y_hat_t = (prob[:5,1] >= 0.3).astype(int)
-    # print(y_hat_t)
-
+    print(probs_val, "\n")
+    print(confusion_matrix(y_val,y_hat), "\n")
+    print(classification_report(y_val,y_hat), "\n")
 main()
