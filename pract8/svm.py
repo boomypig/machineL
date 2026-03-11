bc4a3ce418b017e981bc9e4118fd422141fcb236
Bryan Lopez <brainalexis119@gmail.com>
2026-03-10 23:54:48 -0600

    cleaned up model storing and metrics

--- pract8/svm.py
+++ pract8/svm.py
@@ -20,8 +20,7 @@ TESTING_SIZE = 0.15
 VAL_SIZE = 0.15
 
 def split_data(x,y):
-    print(y.head())
-    x_trainval, x_test,y_trainval,y_test = train_test_split(x,y,train_size=TESTING_SIZE, random_state=RANDOM_SEED, stratify=y)
+    x_trainval,x_test,y_trainval,y_test = train_test_split(x,y,test_size=TESTING_SIZE, random_state=RANDOM_SEED, stratify=y)
 
     x_val_split = VAL_SIZE/(1-TESTING_SIZE)
 
@@ -47,42 +46,24 @@ def pre_proc(x_train):
     ])
     return pre
 
-def evaluate_model(model,x,y,split_name="Validation"):
-    y_pred = model.predict(x)
-
-    metrics = {
-        "Accuracy": accuracy_score(y,y_pred),
-        "Recall": recall_score(y,y_pred),
-        "Precision": precision_score(y,y_pred),
-        "f1": f1_score(y,y_pred),
-    }
-
-    print(f"{split_name.upper()} METRICS")
-    for metric_name, value in metrics.items():
-        print(f"{metric_name:>10} : {value:4f}")
-    print("Confustion Matrix:")
-    print(confusion_matrix(y,y_pred))
-    return y_pred,metrics
 
 def check_dtypes(df):
     columns = df.columns
     for col in columns:
         print(f"\nex of values in col: {col} \n {df[col].head()} \n")
 
+
+
 def train_linear_svm(x_train,y_train,x_val,y_val,c_values):
     print("\n"+"=" * 60)
     print("training linear SVM model")
     print("=" * 60)
     results = []
-    best_model = None
-    best_metrics = None
-    best_pred = None
-    best_f1 = -1
-
-    pre = pre_proc(x_train)
+    models = {}
 
     for c in c_values:
-        print(f"attempting with c: {c}")
+        pre = pre_proc(x_train)
+        # print(f"attempting with c: {c}")
         linear_svc_model = Pipeline(steps=[
             ("pre",pre),
             ("svm",LinearSVC(
@@ -95,31 +76,66 @@ def train_linear_svm(x_train,y_train,x_val,y_val,c_values):
         y_pred, val_metrics = evaluate_model(linear_svc_model,x_val,y_val)
 
         row = {
-            "model": "Linear SVC",
+            "model": "linear svc",
             "C": c,
             "Accuracy":val_metrics["Accuracy"],
             "Recall":val_metrics["Recall"],
             "Precision":val_metrics["Precision"],
-            "f1":val_metrics["f1"]
+            "f1":val_metrics["f1"],
+            "y_pred": y_pred
         }
+        models[c] = linear_svc_model
+
         results.append(row)
-        if val_metrics["f1"] > best_f1:
-            best_model = linear_svc_model
-            best_f1 = val_metrics["f1"]
-            best_pred = y_pred
-            best_metrics = val_metrics
-            
-    return best_model, best_metrics, best_pred, results
+    
+    compare_results_df = pd.DataFrame(results)
+    best_metrics = selecting_best_c(compare_results_df)
+    best_model = models[best_metrics["C"]]
+    best_pred = best_metrics["y_pred"]
+
+    return best_model, best_pred, best_metrics, compare_results_df
 
 
 
+def evaluate_model(model,x,y,split_name="Validation"):
+    y_pred = model.predict(x)
 
+    metrics = {
+        "Accuracy": accuracy_score(y,y_pred),
+        "Recall": recall_score(y,y_pred),
+        "Precision": precision_score(y,y_pred),
+        "f1": f1_score(y,y_pred),
+    }
+
+    # print(f"{split_name.upper()} METRICS")
+    # for metric_name, value in metrics.items():
+    #     print(f"{metric_name:>10} : {value:4f}")
+    # print("Confustion Matrix:")
+    # print(confusion_matrix(y,y_pred))
+    return y_pred,metrics
+
+
+
+def selecting_best_c(results_df, tolerance = 0.01):
+    best_f1 = results_df["f1"].max()
+    contenders = results_df[results_df["f1"] >= best_f1 - tolerance]
+    best_row = contenders.loc[contenders["C"].idxmin()]
+    # best_metrics = {
+    #         "model": best_row["model"],
+    #         "C": best_row["C"],
+    #         "Accuracy":best_row["Accuracy"],
+    #         "Recall":best_row["Recall"],
+    #         "Precision":best_row["Precision"],
+    #         "f1":best_row["f1"],
+    #         "y_pred": best_row["y_pred"]
+    #     }
+    return best_row
 
 
 
 def main():
     df = pd.read_csv("./dataset_svm.csv")
-    check_dtypes(df)
+    # check_dtypes(df)
     TARGET_COLUMN = "churned_next_month"
     DROP_COL = [TARGET_COLUMN, "customer_id"]
 
@@ -139,11 +155,13 @@ def main():
     C_values = np.logspace(-3,3,13)
 
 
-    linear_best_model, linear_best_metrics,linear_best_pred,linear_results = train_linear_svm(x_train,y_train,x_val,y_val,C_values)
+    linear_best_model, linear_best_pred, linear_best_metrics, linear_results_df = train_linear_svm(x_train,y_train,x_val,y_val,C_values)
+    drop_col = ["model","y_pred"]
+    linear_results_df = linear_results_df.drop(columns=drop_col)
+    print(linear_results_df)
 
-    comparison_df = pd.DataFrame(linear_results)
 
-    print(comparison_df.sort_values(by="f1",ascending=False).to_string(index=False))
+    
 
 
 main()
