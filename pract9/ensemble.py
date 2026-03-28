f5ad1fadafe8711b1e12431c473e85fac66c2951
Bryan Lopez <brainalexis119@gmail.com>
2026-03-28 17:39:25 -0600

    bagging

--- pract9/ensemble.py
+++ pract9/ensemble.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from pandas.api.types import is_numeric_dtype
 from sklearn.preprocessing import OneHotEncoder,StandardScaler
 from sklearn.model_selection import train_test_split,RandomizedSearchCV
@@ -8,7 +8,7 @@ from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier,AdaBoostClassifier,GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier,AdaBoostClassifier,GradientBoostingClassifier,BaggingClassifier
 from sklearn.metrics import (
     accuracy_score,
     precision_score,
@@ -26,8 +26,8 @@ def split_data(x,y):
     return x_trainval,x_test,y_trainval,y_test 
 
 def pre_proc(x):
-    num_col = [c for c in x.columns if is_numeric_dtype(x[c])]
-    cat_col = [c for c in x.columns if c not in num_col]
+    num_col = [i for i, c in enumerate(x.columns) if is_numeric_dtype(x[c])]
+    cat_col = [i for i, c in enumerate(x.columns) if not is_numeric_dtype(x[c])]
 
     num_pipe= Pipeline(steps=[
         ("imputer", SimpleImputer(strategy="median")),
@@ -61,7 +61,7 @@ def random_cv(params,pipe):
         estimator=pipe,
         param_distributions=params,
         n_iter=40,
-        scoring="f1",
+        scoring="recall",
         cv=5,
         n_jobs=-1,
         verbose=1,
@@ -72,7 +72,7 @@ def single_tree(x):
     pre = pre_proc(x)
     tree = Pipeline(steps=[
         ("pre",pre),
-        ("clf",DecisionTreeClassifier(random_state=RANDOM_SEED))
+        ("clf",DecisionTreeClassifier(class_weight="balanced",random_state=RANDOM_SEED))
     ])
     return tree
 
@@ -80,13 +80,33 @@ def random_forest(x):
     pre = pre_proc(x)
     forest = Pipeline(steps=[
         ("pre", pre),
-        ("clf",RandomForestClassifier(bootstrap=True,oob_score=True,random_state=RANDOM_SEED))
+        ("clf",RandomForestClassifier(class_weight="balanced",bootstrap=True,oob_score=True,random_state=RANDOM_SEED))
     ])
     return forest
 
 def ada_boost(model):
     ada_clf = AdaBoostClassifier(
-        estimator=model)
+        estimator=model,
+        n_estimators=100,
+        random_state=RANDOM_SEED,
+        learning_rate=0.1,
+        )
+    return ada_clf
+
+def bagg_clf(estimator,x):
+    pre = pre_proc(x)
+    model = Pipeline(steps=[
+        ("pre", pre),
+        ("clf", estimator)
+    ])
+    bagg_clf = BaggingClassifier(
+        estimator=model,
+        n_estimators=100,
+        random_state=RANDOM_SEED,
+        oob_score=True,
+        n_jobs=-1
+    )
+    return bagg_clf
 
 def main():
     df = pd.read_csv("./data/dataset.csv")
@@ -94,33 +114,35 @@ def main():
     DROP_COLUMNS = [TARGET_COLUMN]
     y = df[TARGET_COLUMN]
 
+    results = {}
+    print(y.value_counts())
+    print(y.value_counts(normalize=True)) 
     x = df.drop(columns=DROP_COLUMNS)
-
+    
     x_trainval,x_test,y_trainval,y_test = split_data(x,y)
 
     simple_tree = single_tree(x_trainval)
     
     tree_params = {
         "clf__criterion":["gini","entropy"],
-        "clf__max_depth":list(range(3,11)),
+        "clf__max_depth":list(range(3,15)),
     }
-
+    
     simple_tree_search = random_cv(tree_params,simple_tree)
 
     simple_tree_search.fit(x_trainval,y_trainval)
 
     best_simple_tree = simple_tree_search.best_estimator_
 
-    simple_metrics, pred = evaluate_model(best_simple_tree,x_trainval,y_trainval)
+    simple_metrics, pred = evaluate_model(best_simple_tree,x_test,y_test)
+    results["simple_tree"] = simple_metrics
 
-    simple_tree_scores = pd.DataFrame(simple_tree_search.cv_results_)
-    print(simple_tree_scores.head().sort_values(by="rank_test_score", ascending=True))
+    # simple_tree_scores = pd.DataFrame(simple_tree_search.cv_results_)
+    # print(simple_tree_scores.head().sort_values(by="rank_test_score", ascending=True))
 
-    print(simple_tree_search.best_params_)
+    print("simple tree params:\n",simple_tree_search.best_params_)
 
-    print(simple_metrics)
-
-    print(confusion_matrix(y_trainval,pred))
+    print("simple CM: \n",confusion_matrix(y_test,pred))
     # ----------------------------------------------------------------------------------
     # Random forest
     # ----------------------------------------------------------------------------------
@@ -129,7 +151,7 @@ def main():
     forest_params = {
         "clf__max_depth":list(range(3,15)),
         "clf__criterion":["gini","entropy"],
-        "clf__max_features": ["sqrt","log2","None"]
+        "clf__max_features": ["sqrt","log2",None]
     }
 
     forest_search = random_cv(forest_params,forest)
@@ -138,19 +160,36 @@ def main():
 
     best_forest = forest_search.best_estimator_
 
-    forest_metrics,forest_y_pred = evaluate_model(best_forest,x_trainval,y_trainval)
-    print(forest_metrics)
-    print(confusion_matrix(y_trainval,forest_y_pred))
-    
-    test_forest_metrics, test_forest_y_pred = evaluate_model(best_forest,x_test,y_test)
+    forest_metrics,forest_y_pred = evaluate_model(best_forest,x_test,y_test)
+    results["forest"] = forest_metrics
 
-    print(test_forest_metrics)
-    print(confusion_matrix(y_test,test_forest_y_pred))
+    print("Forest params: \n", forest_search.best_params_)
+
+    print("Forest CM: \n",confusion_matrix(y_test,forest_y_pred))
+    
+    # ----------------------------------------------------------------------------------
+    # bag forest
+    # ----------------------------------------------------------------------------------
+
+    # using best forest to bag 
+    best_forest_est = best_forest.named_steps["clf"]
+
+
+    bagged_forest = bagg_clf(best_forest_est,x_trainval)
+
+    bagged_forest.fit(x_trainval,y_trainval)
+
+
+    bag_metrics, bag_y_pred = evaluate_model(bagged_forest,x_test,y_test)
+
+    results["bagged_forest"] = bag_metrics
+
+    print("Bagged Forest CM: \n", confusion_matrix(y_test,bag_y_pred))
 
     # ----------------------------------------------------------------------------------
     # Ada Boost forest
     # ----------------------------------------------------------------------------------
 
 
-    
+    print(pd.DataFrame(results).T)
 main()
