cd0239a529f644a21e4e94ea4c7346b904316785
Bryan Lopez <brainalexis119@gmail.com>
2026-03-27 20:34:59 -0600

    finish ada and bagging

--- pract9/ensemble.py
+++ pract9/ensemble.py
@@ -7,6 +7,8 @@ from sklearn.model_selection import train_test_split,RandomizedSearchCV
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier,AdaBoostClassifier,GradientBoostingClassifier
 from sklearn.metrics import (
     accuracy_score,
     precision_score,
@@ -59,17 +61,96 @@ def random_cv(params,pipe):
         estimator=pipe,
         param_distributions=params,
         n_iter=40,
-        scoring="roc_auc",
+        scoring="f1",
         cv=5,
         n_jobs=-1,
         verbose=1,
         random_state=RANDOM_SEED
     )
     return search
+def single_tree(x):
+    pre = pre_proc(x)
+    tree = Pipeline(steps=[
+        ("pre",pre),
+        ("clf",DecisionTreeClassifier(random_state=RANDOM_SEED))
+    ])
+    return tree
 
+def random_forest(x):
+    pre = pre_proc(x)
+    forest = Pipeline(steps=[
+        ("pre", pre),
+        ("clf",RandomForestClassifier(bootstrap=True,oob_score=True,random_state=RANDOM_SEED))
+    ])
+    return forest
+
+def ada_boost(model):
+    ada_clf = AdaBoostClassifier(
+        estimator=model)
 
 def main():
     df = pd.read_csv("./data/dataset.csv")
-    print(df.head())
+    TARGET_COLUMN = "target"
+    DROP_COLUMNS = [TARGET_COLUMN]
+    y = df[TARGET_COLUMN]
 
+    x = df.drop(columns=DROP_COLUMNS)
+
+    x_trainval,x_test,y_trainval,y_test = split_data(x,y)
+
+    simple_tree = single_tree(x_trainval)
+    
+    tree_params = {
+        "clf__criterion":["gini","entropy"],
+        "clf__max_depth":list(range(3,11)),
+    }
+
+    simple_tree_search = random_cv(tree_params,simple_tree)
+
+    simple_tree_search.fit(x_trainval,y_trainval)
+
+    best_simple_tree = simple_tree_search.best_estimator_
+
+    simple_metrics, pred = evaluate_model(best_simple_tree,x_trainval,y_trainval)
+
+    simple_tree_scores = pd.DataFrame(simple_tree_search.cv_results_)
+    print(simple_tree_scores.head().sort_values(by="rank_test_score", ascending=True))
+
+    print(simple_tree_search.best_params_)
+
+    print(simple_metrics)
+
+    print(confusion_matrix(y_trainval,pred))
+    # ----------------------------------------------------------------------------------
+    # Random forest
+    # ----------------------------------------------------------------------------------
+    forest = random_forest(x_trainval)
+
+    forest_params = {
+        "clf__max_depth":list(range(3,15)),
+        "clf__criterion":["gini","entropy"],
+        "clf__max_features": ["sqrt","log2","None"]
+    }
+
+    forest_search = random_cv(forest_params,forest)
+
+    forest_search.fit(x_trainval,y_trainval)
+
+    best_forest = forest_search.best_estimator_
+
+    forest_metrics,forest_y_pred = evaluate_model(best_forest,x_trainval,y_trainval)
+    print(forest_metrics)
+    print(confusion_matrix(y_trainval,forest_y_pred))
+    
+    test_forest_metrics, test_forest_y_pred = evaluate_model(best_forest,x_test,y_test)
+
+    print(test_forest_metrics)
+    print(confusion_matrix(y_test,test_forest_y_pred))
+
+    # ----------------------------------------------------------------------------------
+    # Ada Boost forest
+    # ----------------------------------------------------------------------------------
+
+
+    
 main()
